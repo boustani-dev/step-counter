3cf7a53682b1fc62a6ea84f47382f88e6dfa3b92
Mohammad Boostani <122664185+boustani-dev@users.noreply.github.com>
2024-08-04 23:00:51 +0330

    feat: Added functionality for timer action buttons

--- lib/features/stop_watch/components/action_button.dart
+++ lib/features/stop_watch/components/action_button.dart
@@ -17,59 +17,91 @@ class ActionButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<StopWatchBloc, StopWatchState>(
       builder: (context, state) {
-        if (state is StopWatchInitial) {
-          return buildInitialButton();
+        if (state.status is StopWatchInitial) {
+          return buildInitialButton(context);
         }
-        if (state is StopWatchRunning) {
-          return buildPlayingButton();
+        if (state.status is StopWatchRunning) {
+          return buildPlayingButton(context);
         }
-        if (state is StopWatchPaused) {
-          return buildPlayingButton();
+        if (state.status is StopWatchPaused) {
+          return buildPlayingButton(context);
         }
-        if (state is StopWatchStopped) {
-          return buildInitialButton();
+        if (state.status is StopWatchStopped) {
+          return buildInitialButton(context);
         }
-        return Container();
+        return const SizedBox.shrink();
       },
       listener: (context, state) {},
     );
     // return buildPlayingButton();
   }
 
-  Widget buildPlayingButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-            decoration: const BoxDecoration(boxShadow: [
-              BoxShadow(
-                  color: Colors.black38, blurRadius: 6, offset: Offset(0, 0))
-            ], shape: BoxShape.circle, color: AppColors.primary),
-            child: IconButton(
-                onPressed: () {
-                  // context.read<StopWatchBloc>().add(OnStartedTimer(duration));
-                },
-                icon: const Icon(
-                  Icons.play_arrow_rounded,
-                  color: Colors.white,
-                ))),
-        const SizedBox(width: 20),
-        Container(
-            decoration: const BoxDecoration(boxShadow: [
-              BoxShadow(
-                  color: Colors.black38, blurRadius: 6, offset: Offset(0, 0))
-            ], shape: BoxShape.circle, color: AppColors.primary),
-            child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.refresh,
-                  color: Colors.white,
-                ))),
-      ],
+  Widget buildPlayingButton(BuildContext context) {
+    return BlocBuilder<StopWatchBloc, StopWatchState>(
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (state.status is StopWatchStopped ||
+                state.status is StopWatchPaused)
+              Container(
+                  decoration: const BoxDecoration(boxShadow: [
+                    BoxShadow(
+                        color: Colors.black38,
+                        blurRadius: 6,
+                        offset: Offset(0, 0))
+                  ], shape: BoxShape.circle, color: AppColors.primary),
+                  child: IconButton(
+                      onPressed: () {
+                        context
+                            .read<StopWatchBloc>()
+                            .add(const OnResumedTimer());
+                      },
+                      icon: const Icon(
+                        Icons.play_arrow_rounded,
+                        color: Colors.white,
+                      ))),
+            if (state.status is StopWatchRunning)
+              Container(
+                  decoration: const BoxDecoration(boxShadow: [
+                    BoxShadow(
+                        color: Colors.black38,
+                        blurRadius: 6,
+                        offset: Offset(0, 0))
+                  ], shape: BoxShape.circle, color: AppColors.primary),
+                  child: IconButton(
+                      onPressed: () {
+                        context
+                            .read<StopWatchBloc>()
+                            .add(const OnPausedTimer());
+                      },
+                      icon: const Icon(
+                        Icons.pause,
+                        color: Colors.white,
+                      ))),
+            const SizedBox(width: 20),
+            Container(
+                decoration: const BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 6,
+                      offset: Offset(0, 0))
+                ], shape: BoxShape.circle, color: AppColors.primary),
+                child: IconButton(
+                    onPressed: () {
+                      context.read<StopWatchBloc>().add(const OnResetTimer());
+                    },
+                    icon: const Icon(
+                      Icons.refresh,
+                      color: Colors.white,
+                    ))),
+          ],
+        );
+      },
     );
   }
 
-  Widget buildInitialButton() {
+  Widget buildInitialButton(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -77,10 +109,12 @@ class ActionButton extends StatelessWidget {
           decoration: const BoxDecoration(
               shape: BoxShape.circle, color: AppColors.primary),
           child: IconButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<StopWatchBloc>().add(OnStartedTimer(duration));
+            },
             icon: const Icon(
               Icons.play_arrow_rounded,
-              color: AppColors.secondary,
+              color: Colors.white,
             ),
           ),
         ),
